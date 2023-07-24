a9f357135b0045a95f441edbef1d75c88cb441a6
ZahraRzazada <zahraer@code.edu.az>
2023-07-24 14:03:29 +0400

    task.22.07.2023 was added

--- task.22.07.2023/Program.cs
+++ task.22.07.2023/Program.cs
@@ -1,19 +1,24 @@
 ﻿#region TASK1
-//static void CapitalizeSentecne()
-//{
-//    string sentence = "bU tasKI metHodla yAzmAlıSız";
-//    string lowerstr = sentence.ToLower();
-//    String[] arr = lowerstr.Split(' ');
-//    for (int i = 0; i < arr.Length; i++)
-//    {
-//        Console.WriteLine(char.ToUpper(arr[i][0]) + arr[i].Substring(1));
-//    }
-//}
-//CapitalizeSentecne();
+static void CapitalizeSentecne()
+{
+    string sentence = "bU tasKI metHodla yAzmAlıSız";
+    string lowerstr = sentence.ToLower();
+    String[] arr = lowerstr.Split(' ');
+    string capitalize ="";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(char.ToUpper(arr[i][0]) + arr[i].Substring(1)+" ");
+    }
+    //    foreach (var item in arr)
+    //    {
+    //        capitalize += item[0].ToString().ToUpper() + item.Substring(1) + " ";
+    //    }
+    //Console.WriteLine(capitalize);
+}
+CapitalizeSentecne();
 
 
 #endregion
-
 #region TASK2
 //Console.WriteLine("Enter number:");
 //string numberStr = Console.ReadLine();
